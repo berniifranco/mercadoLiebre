2efaf96c2cb506bc144cc028458135b99cbfd80d
Bernardo Franco <berniifranco@gmail.com>
2022-08-29 22:53:35 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.listen(3002, () => {
+    console.log('Servidor corriendo en el puerto 3002');
+});
+
+app.use(express.static(path.join(__dirname, './public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/home.html'));
+});
+
+app.get('/help', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/ayuda.html'));
+});
+
+app.get('/compras', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/compras.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'));
+});
+
+app.get('/ofertas', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/ofertas.html'));
+});
+
+app.get('/registro', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/registro.html'));
+});
+
+app.get('/tiendas', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/tiendas.html'));
+});
+
+app.get('/vender', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/vender.html'));
+});
